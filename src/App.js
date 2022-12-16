81be2355ab851afa5c4558b659b3ab7df1f132d8
luultesfay <luultesfay24@gmail.com>
2022-12-15 17:59:46 -0800

    alternatve way to  context Api and easy way data transfer using useContext HOOK

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+/////transferring data to Components using  Api context  with consumer
+/*
+//way1
 import React, { useState, useEffect } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -43,4 +46,53 @@ function App() {
   );
 }
 
+export default App;
+*/
+
+/////////////////////////Transfering data using  useContect HOOK/////////////////
+//way2
+import React, { useState, useEffect } from "react";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+      }}
+    >
+      <MainHeader onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </AuthContext.Provider>
+  );
+}
 export default App;
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,4 +1,6 @@
-import React from "react";
+//way1
+//data (function and value) receving using ContextApi and consume it using consumer
+/*import React from "react";
 
 import AuthContext from "../../store/auth-context";
 import classes from "./Navigation.module.css";
@@ -30,6 +32,40 @@ const Navigation = () => {
         );
       }}
     </AuthContext.Consumer>
+  );
+};
+
+export default Navigation;
+*/
+//way2
+///alternate and more elegant way passing data using useContext HOOK
+import React, { useContext } from "react";
+
+import AuthContext from "../../store/auth-context";
+import classes from "./Navigation.module.css";
+
+const Navigation = () => {
+  const ctx = useContext(AuthContext);
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={ctx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
